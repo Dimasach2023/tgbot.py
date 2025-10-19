759a7142c03ed96a94023f6a97486bdeb9f00114
Dimasach2023 <230378600+Dimasach2023@users.noreply.github.com>
2025-10-19 18:37:10 +0300

    tgbot.py
    tgbot

--- tgbot.py
+++ tgbot.py
@@ -1,22 +1,26 @@
 import telebot
-
-# Инициализация бота с использованием его токена
+import os
+import random
 bot = telebot.TeleBot("7342064133:AAEhPnefTbiIJJMz49BEqYJnaGo9YCeDWN0")
 
-# Обработчик команды '/start' и '/hello'
-@bot.message_handler(commands=['start', 'hello'])
-def send_welcome(message):
-    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
+@bot.message_handler(commands=['mem'])
+def send_mem(message):
+    names = (os.listdir('images'))
+    name = random.choice(names)
+    with open(f'images/{name}', 'rb') as f:
+        # Отправляем фото, выбранное случайным образом
+        bot.send_photo(message.chat.id, f)
 
-# Обработчик команды '/heh'
-@bot.message_handler(commands=['heh'])
-def send_heh(message):
-    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
-    bot.reply_to(message, "he" * count_heh)
+@bot.message_handler(commands=['animals_mem'])
+def send_mem(message):
+    names1 = (os.listdir('animals_images'))
+    name1 = random.choice(names1)
+    with open(f'animals_images/{name1}', 'rb') as f:
+        # Отправляем фото, выбранное случайным образом
+        bot.send_photo(message.chat.id, f)
 
-@bot.message_handler(commands=['help'])
-def send_welcome(message):
-    bot.reply_to(message, f'Существующие команды: /start /hello - Привет! Я бот; /heh - выводит хех столько раз сколько пользователь указал после команды heh {bot.get_me().first_name}!')
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    bot.send_message(message.chat.id, "Привет! Используй команду /mem, чтобы получить мем!, Используй команду /animals_mem, чтобы получить мем c животными!")
 
-# Запуск бота
 bot.polling()
